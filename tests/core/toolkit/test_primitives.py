cfc8ea8849bf0480081a188de842b67439683b8f
Prashant Gupta <prashantgupta24@gmail.com>
2023-04-20 10:19:43 -0700

    🎨 fmt
    Signed-off-by: Prashant Gupta <prashantgupta24@gmail.com>

--- tests/core/toolkit/test_primitives.py
+++ tests/core/toolkit/test_primitives.py
@@ -1,20 +1,25 @@
-# Local
+# Standard
 from typing import Optional, Union
 
+# Third Party
 import pytest
-from caikit.runtime.service_generation.primitives import (
-    to_output_dm_type,
-)
+
+# Local
+from caikit.runtime.service_generation.primitives import to_output_dm_type
 from tests.fixtures.sample_lib.data_model.sample import SampleOutputType
+
 
 def test_to_output_dm_type_with_dm():
     assert to_output_dm_type(SampleOutputType) == SampleOutputType
 
+
 def test_to_output_dm_type_with_union_dm():
     assert to_output_dm_type(Union[SampleOutputType, str]) == SampleOutputType
 
+
 def test_to_output_dm_type_with_union_optional_dm():
     assert to_output_dm_type(Union[Optional[SampleOutputType], str]) == SampleOutputType
+
 
 def test_to_output_dm_type_raises_primitive():
     with pytest.raises(RuntimeError):
