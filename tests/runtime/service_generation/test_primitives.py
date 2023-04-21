1ad14560c603567a2159bb14f9e6e4e447ba0c55
Prashant Gupta <prashantgupta24@gmail.com>
2023-04-21 10:34:41 -0700

    ✅ add to_primitive_signature tests
    Signed-off-by: Prashant Gupta <prashantgupta24@gmail.com>

--- tests/runtime/service_generation/test_primitives.py
+++ tests/runtime/service_generation/test_primitives.py
@@ -8,8 +8,47 @@ import pytest
 from caikit.runtime.service_generation.primitives import (
     extract_data_model_type_from_union,
     extract_primitive_type_from_union,
+    to_primitive_signature,
 )
 from sample_lib.data_model import SampleInputType, SampleOutputType
+
+
+def test_to_primitive_signature_raw():
+    assert to_primitive_signature(
+        signature={"name": str},
+        primitive_data_model_types=[],
+    ) == {"name": str}
+
+
+def test_to_primitive_signature_union_raw():
+    assert to_primitive_signature(
+        signature={"name": Union[str, int]},
+        primitive_data_model_types=[],
+    ) == {"name": str}
+
+
+def test_to_primitive_signature_dm():
+    assert to_primitive_signature(
+        signature={"name": SampleInputType},
+        primitive_data_model_types=["sample_lib.data_model.SampleInputType"],
+    ) == {"name": SampleInputType}
+
+
+def test_to_primitive_signature_union_dm():
+    assert to_primitive_signature(
+        signature={"name": Union[SampleInputType, str]},
+        primitive_data_model_types=["sample_lib.data_model.SampleInputType"],
+    ) == {"name": SampleInputType}
+
+
+def test_to_primitive_signature_no_primitive():
+    assert (
+        to_primitive_signature(
+            signature={"name": SampleInputType},
+            primitive_data_model_types=[],
+        )
+        == {}
+    )
 
 
 def test_to_output_dm_type_with_dm():
