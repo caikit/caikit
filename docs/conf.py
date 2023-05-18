e24d1a098ff922df1c37155297fcd570bbcb922e
Martin Hickey <martin.hickey@ie.ibm.com>
2023-05-18 16:30:14 +0100

    Fix formatting
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -6,20 +6,22 @@
 
 # -- Path setup --------------------------------------------------------------
 
+# Standard
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Caikit'
-copyright = '2023, The Caikit Authors'
-author = 'The Caikit Authors'
+project = "Caikit"
+copyright = "2023, The Caikit Authors"
+author = "The Caikit Authors"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,24 +30,24 @@ author = 'The Caikit Authors'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-]   
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'caikit/config']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "caikit/config"]
 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
- # a list of builtin themes.
- #
+# a list of builtin themes.
+#
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
