f3f69944af470cc1e52261f1d801b63d8e8f4ce1
Joe Runde <Joseph.Runde@ibm.com>
2023-06-08 14:11:06 -0600

    :test_tube: create test streaming module
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>

---
+++ tests/fixtures/sample_lib/modules/sample_task/streaming_module.py
@@ -0,0 +1,56 @@
+"""
+A sample module for sample things!
+"""
+# Standard
+import os
+
+# Local
+from ...data_model.sample import (
+    SampleInputType,
+    SampleOutputType,
+    StreamingTask,
+)
+from caikit.core.data_model import DataStream
+from caikit.core.modules import ModuleLoader, ModuleSaver
+import caikit.core
+
+
+@caikit.core.module(
+    "00110203-0123-0456-0789-0a0b02dd0e0f", "SampleModule", "0.0.1", StreamingTask
+)
+class StreamingModule(caikit.core.ModuleBase):
+
+    def __init__(self, stream_size):
+        super().__init__()
+        self.stream_size = stream_size
+
+    @classmethod
+    def load(cls, model_path, **kwargs):
+        loader = ModuleLoader(model_path)
+        config = loader.config
+        return cls(config["stream_size"])
+
+    def run(
+        self, sample_input: SampleInputType
+    ) -> DataStream[SampleOutputType]:
+        """
+        Args:
+            sample_input (sample_lib.data_model.SampleInputType): the input
+
+        Returns:
+            sample_lib.data_model.SampleOutputType: The output
+        """
+        list_ = [SampleOutputType(f"Hello {sample_input.name}") for x in range(self.stream_size)]
+        stream = DataStream.from_iterable(list_)
+        return stream
+
+    def save(self, model_path):
+        module_saver = ModuleSaver(
+            self,
+            model_path=model_path,
+        )
+        with module_saver:
+            config_options = {
+                "stream_size": self.stream_size
+            }
+            module_saver.update_config(config_options)
