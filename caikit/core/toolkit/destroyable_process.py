8ea361c3711d6b747519c9a1e2662877ff188b3c
Gabe Goodhart <ghart@us.ibm.com>
2023-07-07 13:59:05 -0600

    TrainFutures: Add DestroyableProcess
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

---
+++ caikit/core/toolkit/destroyable_process.py
@@ -0,0 +1,193 @@
+# Copyright The Caikit Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+A DestroyableProcess implements a multiprocessing Process that captures errors
+and communicates them to its parent with clean semantics for being destroyed.
+
+NOTE: The "fork" start method is used for two reasons:
+    1. It's faster
+    2. For auto-generated classes (e.g. stream sources), "spawn" can result in
+        missing classes since it performs a full re-import, but may not
+        regenerate these classes.
+"""
+# Standard
+from functools import partial
+from typing import Any, Callable, Optional, Tuple
+import multiprocessing
+
+# First Party
+import alog
+
+# Local
+from .destroyable import Destroyable
+
+log = alog.use_channel("DESTROY-PROC")
+
+FORK_CTX = multiprocessing.get_context("fork")
+
+
+class DestroyableProcess(
+    FORK_CTX.Process, Destroyable
+):  # pylint: disable=too-many-instance-attributes
+    __doc__ = __doc__
+
+    def __init__(
+        self,
+        target: Optional[Callable] = None,
+        completion_event: Optional[multiprocessing.Event] = None,
+        args: Optional[Tuple] = None,
+        kwargs: Optional[dict] = None,
+        destroy_grace_period: float = 10,
+        return_result: bool = True,
+        **_kwargs,
+    ):
+        """Initialize with an event to use to signal completion"""
+        self._parent_conn, self._child_conn = FORK_CTX.Pipe()
+        self._completion_event = completion_event or FORK_CTX.Event()
+        self._destroy_grace_period = destroy_grace_period
+        self._return_result = return_result
+
+        # This will hold the terminal result of the process which will either be
+        # an Exception or some other value
+        self.__result = None
+
+        # Descriptions of completion state
+        self.__started = False
+        self.__destroyed = False
+        self.__canceled = False
+
+        # Bind the target to the target wrapper
+        wrapped_target = partial(
+            self._target_wrapper,
+            target=target,
+            args=args,
+            kwargs=kwargs,
+        )
+        super().__init__(target=wrapped_target, **_kwargs)
+
+    ## Destroyable Interface ##
+
+    @property
+    def destroyed(self) -> bool:
+        return self.__destroyed
+
+    @property
+    def canceled(self) -> bool:
+        return self.__canceled
+
+    @property
+    def ran(self) -> bool:
+        return self.__started and not self.is_alive()
+
+    @property
+    def threw(self) -> bool:
+        return self.error is not None
+
+    def get_or_throw(self) -> Any:
+        """Get the result of the execution or raise an error if one occurred"""
+        if self.destroyed:
+            log.error(
+                "<COR24981767E>",
+                "get_or_throw called on destroyed process, no value to return",
+            )
+
+        if not self.ran:
+            log.error(
+                "<COR12037430E>",
+                "get_or_throw called on process, but it has not finished running",
+            )
+
+        # Update the result and throw if it's an error
+        error = self.error
+        if error is not None:
+            raise error
+        return self.__result
+
+    def destroy(self):
+        """Cancel any in-progress work"""
+        self.__destroyed = True
+        if self.is_alive() or not self.__started:
+            self.__canceled = True
+        if self.__started:
+            self.terminate()
+            self.join(self._destroy_grace_period)
+            self.kill()
+            self.join()
+            self._completion_event.set()
+
+    ## Process Interface ##
+
+    def start(self):
+        if self.destroyed:
+            err_msg = "Not starting work on pre-canceled process"
+            log.warning("<COR42191929W>", err_msg)
+            self._completion_event.set()
+            self.__result = RuntimeError(err_msg)
+            return
+        self.__started = True
+        return super().start()
+
+    def join(self, *args, **kwargs):
+        if self.destroyed and not self.__started:
+            return
+        return super().join(*args, **kwargs)
+
+    # NOTE: This functionality is covered by the unit tests, but pytest-cov is
+    #   not correctly collecting the coverage info since it executes inside the
+    #   subprocess
+    def run(self):  # pragma: no cover
+        try:
+            # Run and indicate to the parent that no
+            super().run()
+
+        # Catch any errors thrown within a subprocess so that they can be
+        # forwarded to the parent
+        # pylint: disable=broad-exception-caught
+        except Exception as err:
+            err_str = repr(err)
+            log.error(
+                "<COR69863806E>",
+                "Caught exception in destroyable process: %s",
+                err_str,
+                exc_info=True,
+            )
+            self._child_conn.send(err)
+        finally:
+            self._completion_event.set()
+
+    ## Impl ##
+
+    def _update_result(self):
+        if self._parent_conn.poll():
+            self.__result = self._parent_conn.recv()
+
+    @property
+    def error(self) -> Optional[Exception]:
+        self._update_result()
+        if isinstance(self.__result, Exception):
+            return self.__result
+
+    @property
+    def completion_event(self) -> multiprocessing.Event:
+        return self._completion_event
+
+    # NOTE: This functionality is covered by the unit tests, but pytest-cov is
+    #   not correctly collecting the coverage info since it executes inside the
+    #   subprocess
+    def _target_wrapper(self, target, args, kwargs):  # pragma: no cover
+        result = target(*(args or []), **(kwargs or {}))
+        log.debug3("Process target result: %s", result)
+        if self._return_result:
+            self._child_conn.send(result)
+        self._completion_event.set()
---
+++ tests/core/toolkit/test_destroyable_process.py
@@ -0,0 +1,138 @@
+# Copyright The Caikit Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Standard
+import time
+
+# Third Party
+import pytest
+
+# Local
+from caikit.core.toolkit.destroyable_process import FORK_CTX, DestroyableProcess
+
+
+def test_processes_can_be_interrupted():
+    def infinite_wait():
+        while True:
+            time.sleep(0.1)
+
+    proc = DestroyableProcess(infinite_wait)
+    proc.start()
+    assert not proc.destroyed
+    assert not proc.canceled
+    assert not proc.ran
+    assert not proc.threw
+    proc.destroy()
+    proc.join(60)
+    assert not proc.is_alive()
+    assert proc.destroyed
+    assert proc.canceled
+    assert proc.ran
+    assert not proc.threw
+
+
+def test_processes_can_return_results():
+    expected = "test-any-result"
+    proc = DestroyableProcess(lambda: expected)
+    proc.start()
+    proc.join()
+    assert expected == proc.get_or_throw()
+    assert not proc.destroyed
+    assert not proc.canceled
+    assert proc.ran
+    assert not proc.threw
+
+
+def test_processes_can_be_set_to_not_return_results():
+    expected = "test-any-result"
+    proc = DestroyableProcess(lambda: expected, return_result=False)
+    proc.start()
+    proc.join()
+    assert proc.get_or_throw() is None
+    assert not proc.destroyed
+    assert not proc.canceled
+    assert proc.ran
+    assert not proc.threw
+
+
+def test_processes_can_throw():
+    expected = ValueError("test-any-error")
+
+    def thrower():
+        raise expected
+
+    proc = DestroyableProcess(thrower)
+    proc.start()
+    proc.join()
+    assert not proc.destroyed
+    assert not proc.canceled
+    assert proc.ran
+    assert proc.threw
+
+    with pytest.raises(ValueError) as ctx:
+        proc.get_or_throw()
+
+    assert str(expected) == str(ctx.value)
+
+
+def test_processes_will_not_execute_if_destroyed_before_starting():
+    proc = DestroyableProcess(lambda: time.sleep(1000))
+    proc.destroy()
+    proc.start()
+    assert not proc.is_alive()
+    proc.join()
+    with pytest.raises(RuntimeError):
+        proc.get_or_throw()
+    assert proc.destroyed
+    assert proc.canceled
+    assert not proc.ran
+    assert proc.threw
+
+
+def test_event_is_set_on_completion():
+    event = FORK_CTX.Event()
+    proc = DestroyableProcess(lambda: None, completion_event=event)
+    assert not event.is_set()
+    proc.start()
+    proc.join()
+    assert event.is_set()
+    assert not proc.destroyed
+    assert not proc.canceled
+    assert proc.ran
+    assert not proc.threw
+
+
+def test_event_is_set_on_exception():
+    event = FORK_CTX.Event()
+
+    def thrower():
+        raise ValueError("test-any-exception")
+
+    proc = DestroyableProcess(thrower, completion_event=event)
+    assert not event.is_set()
+    proc.start()
+    proc.join()
+    assert event.is_set()
+    assert not proc.destroyed
+    assert not proc.canceled
+    assert proc.ran
+    assert proc.threw
+
+
+def test_default_event_is_set_on_completion():
+    proc = DestroyableProcess(lambda: None)
+    assert not proc.completion_event.is_set()
+    proc.start()
+    proc.join()
+    assert proc.completion_event.is_set()
