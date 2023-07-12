cac88b46c725b142c7ca0c7c5d7a26dc6716bef4
Joe Runde <Joseph.Runde@ibm.com>
2023-07-12 17:19:09 -0600

    :sparkles: add http example
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>

--- examples/text-sentiment/client.py
+++ examples/text-sentiment/client.py
@@ -13,30 +13,54 @@
 # limitations under the License.
 
 # Third Party
+from start_runtime import protocol_arg
 import grpc
+import requests
 
 # Local
 from caikit.runtime.service_factory import ServicePackageFactory
 from text_sentiment.data_model import TextInput
 
-inference_service = ServicePackageFactory().get_service_package(
-    ServicePackageFactory.ServiceType.INFERENCE,
-)
+if __name__ == "__main__":
 
-port = 8085
-
-# Setup the client
-channel = grpc.insecure_channel(f"localhost:{port}")
-client_stub = inference_service.stub_class(channel)
-
-# Run inference for two sample prompts
-for text in ["I am not feeling well today!", "Today is a nice sunny day"]:
-    input_text_proto = TextInput(text=text).to_proto()
-    request = inference_service.messages.HuggingFaceSentimentTaskRequest(
-        text_input=input_text_proto
+    inference_service = ServicePackageFactory().get_service_package(
+        ServicePackageFactory.ServiceType.INFERENCE,
     )
-    response = client_stub.HuggingFaceSentimentTaskPredict(
-        request, metadata=[("mm-model-id", "text_sentiment")]
-    )
-    print("Text:", text)
-    print("RESPONSE:", response)
+
+    protocol = protocol_arg()
+
+    model_id = "text_sentiment"
+
+    if protocol == "grpc":
+        # Setup the client
+        port = 8085
+        channel = grpc.insecure_channel(f"localhost:{port}")
+        client_stub = inference_service.stub_class(channel)
+
+        # Run inference for two sample prompts
+        for text in ["I am not feeling well today!", "Today is a nice sunny day"]:
+            input_text_proto = TextInput(text=text).to_proto()
+            request = inference_service.messages.HuggingFaceSentimentTaskRequest(
+                text_input=input_text_proto
+            )
+            response = client_stub.HuggingFaceSentimentTaskPredict(
+                request, metadata=[("mm-model-id", model_id)], timeout=1
+            )
+            print("Text:", text)
+            print("RESPONSE:", response)
+
+    elif protocol == "http":
+        port = 8080
+        # Run inference for two sample prompts
+        for text in ["I am not feeling well today!", "Today is a nice sunny day"]:
+            payload = {"inputs": {"text_input": {"text": text}}}
+            response = requests.post(
+                f"http://localhost:{port}/api/v1/{model_id}/task/hugging-face-sentiment",
+                json=payload,
+                timeout=1,
+            )
+            print("Text:", text)
+            print("RESPONSE:", response.json())
+
+    else:
+        print("--protocol must be one of [grpc, http]")
