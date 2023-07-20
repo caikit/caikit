16b06fb555ab5c1270374700b0a001cbab7dc410
Angel Luu <angel.luu@us.ibm.com>
2023-07-20 17:11:14 -0600

    Uncomment some tests and fix
    Signed-off-by: Angel Luu <angel.luu@us.ibm.com>

--- caikit/core/data_model/base.py
+++ caikit/core/data_model/base.py
@@ -759,21 +759,21 @@ class DataBase(metaclass=_DataBaseMetaClass):
         # setting union of list type "foo" to "foo_str_sequence"
         if cls._fields_oneofs_map:
             json_as_dict = json.loads(json_str)
-            for one_of, union_list_types in cls._fields_oneofs_map.items():
+            for oneof, union_list_types in cls._fields_oneofs_map.items():
                 # if one_of is in json_as_dict, this one_of is a union of lists
-                if one_of in json_as_dict:
-                    value = json_as_dict[one_of]
+                if oneof in json_as_dict:
+                    value = json_as_dict[oneof]
                     if isinstance(value, list):
                         union_type = (
-                            f"{one_of}_{type(value[0]).__name__}_sequence"
+                            f"{oneof}_{type(value[0]).__name__}_sequence"
                             if len(value) > 0
                             else union_list_types[0]
                         )
                         json_as_dict[union_type] = {"values": value}
                     else:
-                        union_type = f"{one_of}_{type(value).__name__}"
+                        union_type = f"{oneof}_{type(value).__name__}"
                         json_as_dict[union_type] = value
-                    del json_as_dict[one_of]
+                    del json_as_dict[oneof]
 
             # put the new json_as_dict back into json_str
             json_str = json.dumps(json_as_dict)
@@ -961,14 +961,19 @@ class DataBase(metaclass=_DataBaseMetaClass):
         dict_val = self.to_dict()
 
         # if this class has union of lists, converting the union list field into the one_of
-        # Ex: converting dict from {"foo_str_sequence": ["a","b"]} to {"foo": ["a","b"]}
+        # Ex: converting dict from {"foo_str_sequence": {"values": ["a","b"]}} to {"foo": ["a","b"]}
         if self._fields_oneofs_map:
             for one_of, fields in self._fields_oneofs_map.items():
                 # make sure that this one_of is a union of lists
                 if any(field.endswith("_sequence") for field in fields):
                     for field in fields:
                         if field in dict_val:
-                            dict_val[one_of] = dict_val[field]
+                            field_val = dict_val[field]
+                            dict_val[one_of] = (
+                                field_val["values"]
+                                if isinstance(field_val, dict)
+                                else field_val
+                            )
                             del dict_val[field]
 
         return json.dumps(dict_val, **kwargs)
