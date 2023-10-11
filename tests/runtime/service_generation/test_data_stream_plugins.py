d796bfe9c5ebd1cccc422972e34b025425d30f1f
Gabe Goodhart <ghart@us.ibm.com>
2023-10-10 21:29:29 -0600

    PluggableDataIO: Unit tests for plugin factory
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- tests/runtime/service_generation/test_data_stream_plugins.py
+++ tests/runtime/service_generation/test_data_stream_plugins.py
@@ -11,17 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Tests for the plugin mechanism for data stream sources
+"""
+# Third Party
+import pytest
+
+# First Party
+import aconfig
+
 # Local
+from caikit.core.data_model.streams.data_stream import DataStream
 from caikit.interfaces.common.data_model.stream_sources import (
     Directory,
     File,
     ListOfFiles,
 )
 from caikit.runtime.service_generation.data_stream_source import (
+    DataStreamPluginFactory,
+    DataStreamSourcePlugin,
     DirectoryDataStreamSourcePlugin,
     FileDataStreamSourcePlugin,
     JsonDataStreamSourcePlugin,
     ListOfFilesDataStreamSourcePlugin,
+    S3FilesDataStreamSourcePlugin,
+    make_data_stream_source,
 )
 from tests.runtime.service_generation.test_data_stream_source import (
     validate_data_stream,
@@ -92,3 +106,114 @@ def test_json_data_plugin_can_be_reinitialized():
     )
 
     assert strm_type is strm_type2
+
+
+## PluginFactory ###############################################################
+
+
+@pytest.fixture
+def plugin_factory():
+    fct = DataStreamPluginFactory("TestFactory")
+    fct.register(JsonDataStreamSourcePlugin)
+    fct.register(FileDataStreamSourcePlugin)
+    fct.register(ListOfFilesDataStreamSourcePlugin)
+    fct.register(DirectoryDataStreamSourcePlugin)
+    fct.register(S3FilesDataStreamSourcePlugin)
+    yield fct
+
+
+class SampleTrainingPlugin(DataStreamSourcePlugin):
+    """Dummy plugin that can be registered with a factory"""
+
+    name = "TEST"
+
+    def get_stream_message_type(self, *_, **__):
+        return sample_lib.data_model.SampleTrainingType
+
+    def to_data_stream(self, source_message, *_, **__):
+        count = self._config.get("count", 5)
+        DataStream.from_iterable(count * [source_message])
+
+    def get_field_number(self) -> int:
+        return self._config.get("field_number", 42)
+
+    def get_field_name(self, element_type):
+        return self._config.get("field_name", super().get_field_name(element_type))
+
+
+def test_plugin_factory_is_configurable(plugin_factory):
+    """Make sure new plugins can be registered and configured"""
+    cfg = aconfig.Config(
+        {"foo": {"type": SampleTrainingPlugin.name}},
+        override_env_vars=False,
+    )
+    # Before registration, it's a ValueError
+    with pytest.raises(ValueError):
+        plugin_factory.construct(cfg.foo, "foo")
+
+    # After registration, it's valid
+    plugin_factory.register(SampleTrainingPlugin)
+    plug = plugin_factory.construct(cfg.foo, "foo")
+    assert isinstance(plug, SampleTrainingPlugin)
+
+    # Make sure there's an instance in get_plugins
+    all_plugins = plugin_factory.get_plugins(cfg)
+    assert any(isinstance(plug, SampleTrainingPlugin) for plug in all_plugins)
+
+
+def test_plugin_factory_reuses_plugins(plugin_factory):
+    """Make sure that the list of plugins gets reused"""
+    plugins = plugin_factory.get_plugins()
+    assert plugin_factory.get_plugins() is plugins
+
+
+def test_no_duplicate_field_numbers(plugin_factory):
+    """Make sure that duplicate field numbers cause a ValueError"""
+    plugin_factory.register(SampleTrainingPlugin)
+    cfg = aconfig.Config(
+        {
+            "foo": {"type": SampleTrainingPlugin.name},
+            "bar": {"type": SampleTrainingPlugin.name},
+        },
+        override_env_vars=False,
+    )
+    with pytest.raises(ValueError):
+        plugin_factory.get_plugins(cfg)
+
+
+def test_no_duplicate_field_names(plugin_factory):
+    """Make sure that new data stream sources cannot be created with duplicate
+    field names
+    """
+    plugin_factory.register(SampleTrainingPlugin)
+    cfg = aconfig.Config(
+        {
+            "foo": {"type": SampleTrainingPlugin.name, "config": {"field_number": 42}},
+            "bar": {"type": SampleTrainingPlugin.name, "config": {"field_number": 43}},
+        },
+        override_env_vars=False,
+    )
+    with pytest.raises(ValueError):
+        make_data_stream_source(int, plugin_factory, cfg)
+
+
+def test_multiple_instances(plugin_factory):
+    """Make sure that multiple instances of a type with different configs can be
+    instantiated as long as field numbers and names are unique
+    """
+    plugin_factory.register(SampleTrainingPlugin)
+    cfg = aconfig.Config(
+        {
+            "foo": {
+                "type": SampleTrainingPlugin.name,
+                "config": {"field_number": 42, "field_name": "foo"},
+            },
+            "bar": {
+                "type": SampleTrainingPlugin.name,
+                "config": {"field_number": 43, "field_name": "bar"},
+            },
+        },
+        override_env_vars=False,
+    )
+    plugin_factory.get_plugins(cfg)
+    make_data_stream_source(int, plugin_factory, cfg)
