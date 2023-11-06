dd556bd24b560f7513edaef69a193309f5c76704
Joe Runde <joe@joerun.de>
2023-11-06 16:56:30 -0700

    :art: use walrus operator
    Co-authored-by: Gabe Goodhart <gabe.l.hart@gmail.com>
    Signed-off-by: Joe Runde <joe@joerun.de>

--- caikit/runtime/servicers/global_train_servicer.py
+++ caikit/runtime/servicers/global_train_servicer.py
@@ -216,12 +216,11 @@ class GlobalTrainServicer:
         ).from_proto(request)
 
         # Create the model_saver to handle saving the training output
-        model_saver = (
+        if model_saver := (
             request_data_model.output_target.make_model_saver()
             if request_data_model.output_target
             else None
-        )
-        if model_saver is None:
+        ) is None:
             # No output_target was supplied, so fall back to a configured local save path.
             # Use either:
             # 1. The provided `training_output_dir` here, or
