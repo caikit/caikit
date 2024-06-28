61cd454cb6b51878556f9349bd8f1dbbd53732a9
gkumbhat <kumbhat.gaurav@gmail.com>
2024-06-28 16:56:29 -0500

    :truck: Move caikit rps streaming check in caikit_rpc condition
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- caikit/runtime/interceptors/caikit_runtime_server_wrapper.py
+++ caikit/runtime/interceptors/caikit_runtime_server_wrapper.py
@@ -142,13 +142,15 @@ class CaikitRuntimeServerWrapper(grpc.Server):
             with alog.ContextLog(log.debug, "[Safe RPC]: %s", rpc.__name__):
                 try:
                     IN_PROGRESS_GAUGE.labels(rpc_name=rpc.__name__).inc()
-                    # Enable sending acknowledgement for bi-directional streaming cases
-                    # Note: we are not enabling it for every rpc, since it may create confusion
-                    # on client side
-                    if caikit_rpc._input_streaming and caikit_rpc._output_streaming:
-                        # Send an acknowledgement in metadata
-                        context.send_initial_metadata(((ACK_STRING, "ok"),))
                     if caikit_rpc:
+
+                        # Enable sending acknowledgement for bi-directional streaming cases
+                        # Note: we are not enabling it for every rpc, since it may create confusion
+                        # on client side
+                        if caikit_rpc._input_streaming and caikit_rpc._output_streaming:
+                            # Send an acknowledgement in metadata
+                            context.send_initial_metadata(((ACK_STRING, "ok"),))
+                            
                         # Pass through the CaikitRPCBase rpc description to the global handlers
                         return rpc(request, context, caikit_rpc=caikit_rpc)
                     return rpc(request, context)
