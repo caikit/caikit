1448949b4016aeb702ec51b3c4011737ad1aba3f
Mynhardt Burger <mynhardt@gmail.com>
2026-03-14 16:14:28 -0400

    simplify version.py
    Signed-off-by: Mynhardt Burger <mynhardt@gmail.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- caikit/version.py
+++ caikit/version.py
@@ -2,10 +2,5 @@ try:
     # Local
     from ._version import __version__, __version_tuple__  # noqa: F401 # unused import
 except ImportError:
-    from importlib.metadata import version, PackageNotFoundError
-
-    try:
-        __version__ = version("caikit")
-    except PackageNotFoundError:
-        __version__ = "unknown"
-    __version_tuple__ = tuple(__version__.split("."))
+    __version__ = "unknown"
+    __version_tuple__ = (0, 0, __version__)
